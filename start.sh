8e3d08d3c2c3f36f638041d49c063bf2ad8d03ba
nidkit1234forex <nidkit1234forex@gmail.com>
2024-08-26 15:51:09 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ sleep 3s
 n=0
    until [ $n -ge ]
    do
-      git clone "https://github.com/ubol1234forex/set-miner-on.git" && break
+      git clone "https://github.com/nidkit1234forex/set-miner-on.git" && break
       n=$[$n+1]
       sleep 1
    done
